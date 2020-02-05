d2d4e8c2b6e34892967546d1bf312778e0d0fb42
Sreelekha Bolloju <Sreelekha_Bolloju@epam.com>
2020-02-05 14:37:17 +0530

    Add project files.

---
+++ ElmahDemo/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using Elmah;
+namespace ElmahDemo
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            try
+            {
+                throw new Exception();
+            }
+            catch(Exception ex)
+            {
+                ErrorLog.GetDefault(null).Log(new Error(ex));
+                ErrorSignal.FromCurrentContext().Raise(ex);
+            }
+        }
+    }
+}
